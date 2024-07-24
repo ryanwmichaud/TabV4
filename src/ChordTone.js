de1e329604e9cba7785a72946056c1f370687639
ryanwmichaud <92121030+ryanwmichaud@users.noreply.github.com>
2024-07-24 16:46:25 -0500

    resize checkboxes wrap w lables

--- src/ChordTone.js
+++ src/ChordTone.js
@@ -23,34 +23,55 @@ class ChordToneInput extends React.Component{
       return(
         <div className='ctinput'> 
               <legend>Chord Tones: </legend>
-                <input type='checkbox' id='0' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="0" class="checkbox-label">C</label>              
-                <input type='checkbox' id='1' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="1" class="checkbox-label">C#</label>
-                <input type='checkbox' id='2' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="2" class="checkbox-label">D</label>
-                <input type='checkbox' id='3' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="3" class="checkbox-label">D#</label>
-                <input type='checkbox' id='4' name='ct' class="checkbox-input" onClick={this.handleClick} />
-                <label for="4" class="checkbox-label">E</label>
-                <input type='checkbox' id='5' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="5" class="checkbox-label">F</label>
-                <input type='checkbox' id='6' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="6" class="checkbox-label">F#</label>
-                <input type='checkbox' id='7' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="7" class="checkbox-label">G</label>
-                <input type='checkbox' id='8' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="8" class="checkbox-label">G#</label>
-                <input type='checkbox' id='9' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="9" class="checkbox-label">A</label>
-                <input type='checkbox' id='10' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="10" class="checkbox-label">A#</label>
-                <input type='checkbox' id='11' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="11" class="checkbox-label">B</label>
-              
-              
-         
-              
+              <div class="ct-container">
+                <input type='checkbox' id='0' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="0" class="ct-label">C</label>
+              </div>
+              <div class="ct-container">
+                <input type='checkbox' id='1' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="1" class="ct-label">C#</label>
+              </div>
+              <div class="ct-container">
+                <input type='checkbox' id='2' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="2" class="ct-label">D</label>
+              </div>
+              <div class="ct-container">
+                <input type='checkbox' id='3' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="3" class="ct-label">D#</label>
+              </div>
+              <div class="ct-container">
+                <input type='checkbox' id='4' name='ct' class="ct-input" onClick={this.handleClick} />
+                <label for="4" class="ct-label">E</label>
+              </div>
+              <div class="ct-container">
+                <input type='checkbox' id='5' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="5" class="ct-label">F</label>
+              </div>
+              <div class="ct-container">
+                <input type='checkbox' id='6' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="6" class="ct-label">F#</label>
+              </div>
+              <div class="ct-container">
+                <input type='checkbox' id='7' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="7" class="ct-label">G</label>
+              </div>
+              <div class="ct-container">
+                <input type='checkbox' id='8' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="8" class="ct-label">G#</label>
+              </div>
+              <div class="ct-container">
+                <input type='checkbox' id='9' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="9" class="ct-label">A</label>
+              </div>
+              <div class="ct-container">
+                <input type='checkbox' id='10' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="10" class="ct-label">A#</label>
+              </div>
+              <div class="ct-container">
+                <input type='checkbox' id='11' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="11" class="ct-label">B</label>
+              </div>
+                
               
               
         </div>
@@ -66,53 +87,31 @@ class ChordToneInput extends React.Component{
   export {ChordToneInput}
 
   /*
-  <div class="checkbox-container">
-                <label for="0" class="checkbox-label">C</label>
-              </div>
-              <div class="checkbox-container">
-                <input type='checkbox' id='1' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="1" class="checkbox-label">C#</label>
-              </div>
-              <div class="checkbox-container">
-                <input type='checkbox' id='2' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="2" class="checkbox-label">D</label>
-              </div>
-              <div class="checkbox-container">
-                <input type='checkbox' id='3' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="3" class="checkbox-label">D#</label>
-              </div>
-              <div class="checkbox-container">
-                <input type='checkbox' id='4' name='ct' class="checkbox-input" onClick={this.handleClick} />
-                <label for="4" class="checkbox-label">E</label>
-              </div>
-              <div class="checkbox-container">
-                <input type='checkbox' id='5' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="5" class="checkbox-label">F</label>
-              </div>
-              <div class="checkbox-container">
-                <input type='checkbox' id='6' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="6" class="checkbox-label">F#</label>
-              </div>
-              <div class="checkbox-container">
-                <input type='checkbox' id='7' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="7" class="checkbox-label">G</label>
-              </div>
-              <div class="checkbox-container">
-                <input type='checkbox' id='8' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="8" class="checkbox-label">G#</label>
-              </div>
-              <div class="checkbox-container">
-                <input type='checkbox' id='9' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="9" class="checkbox-label">A</label>
-              </div>
-              <div class="checkbox-container">
-                <input type='checkbox' id='10' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="10" class="checkbox-label">A#</label>
-              </div>
-              <div class="checkbox-container">
-                <input type='checkbox' id='11' name='ct' class="checkbox-input" onClick={this.handleClick}/>
-                <label for="11" class="checkbox-label">B</label>
-              </div>
+                <input type='checkbox' id='0' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="0" class="ct-label">C</label>              
+                <input type='checkbox' id='1' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="1" class="ct-label">C#</label>
+                <input type='checkbox' id='2' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="2" class="ct-label">D</label>
+                <input type='checkbox' id='3' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="3" class="ct-label">D#</label>
+                <input type='checkbox' id='4' name='ct' class="ct-input" onClick={this.handleClick} />
+                <label for="4" class="ct-label">E</label>
+                <input type='checkbox' id='5' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="5" class="ct-label">F</label>
+                <input type='checkbox' id='6' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="6" class="ct-label">F#</label>
+                <input type='checkbox' id='7' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="7" class="ct-label">G</label>
+                <input type='checkbox' id='8' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="8" class="ct-label">G#</label>
+                <input type='checkbox' id='9' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="9" class="ct-label">A</label>
+                <input type='checkbox' id='10' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="10" class="ct-label">A#</label>
+                <input type='checkbox' id='11' name='ct' class="ct-input" onClick={this.handleClick}/>
+                <label for="11" class="ct-label">B</label>
+              
   
   
   */
