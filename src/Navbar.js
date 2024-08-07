abeb6d4b66f73fa42ef4c88e2f1ba213403926db
ryanwmichaud <92121030+ryanwmichaud@users.noreply.github.com>
2024-08-06 21:30:47 -0400

    navbar work

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,1 +1,21 @@
 
+import React from "react";
+import { Link } from 'react-router-dom';
+
+
+
+
+function Navbar(){
+
+    return(
+        <div className="navbar">  
+          <Link className='title' to={"/"}> Fretbchord Explchorder </Link>
+          <Link className="other" to={"/gallery"}>  Gallery</Link>
+          <Link className='login' to={"/Login"}> Login </Link> 
+
+        </div>
+    );
+
+}
+
+export default Navbar;
