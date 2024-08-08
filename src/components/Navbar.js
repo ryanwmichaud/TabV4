c6a1f9d2d340c81c9e08f8ddc6a45fb3c3401139
ryanwmichaud <92121030+ryanwmichaud@users.noreply.github.com>
2024-08-08 10:13:58 -0400

    login/out from nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,19 @@
 
-import React from "react";
+import React, {useContext} from "react";
 import { Link  } from 'react-router-dom';
 import { MenuButton } from "./MenuButton";
 import { useLocation } from "react-router-dom";
+import { GlobalContext } from "../App";
+import { googleLogout } from "@react-oauth/google";
 
 
 
 
-const Navbar = ({isMobileMenuVisible, setIsMobileMenuVisible, isMobileView}) => {
+const Navbar = () => {
 
     const location = useLocation();
+    const {profile, setProfile, isMobileView, isMobileMenuVisible, setIsMobileMenuVisible} = useContext(GlobalContext)
+
 
     
 
@@ -25,7 +29,19 @@ const Navbar = ({isMobileMenuVisible, setIsMobileMenuVisible, isMobileView}) => 
 
         } 
           <Link className='title' to={"/"}> Fretbchord Explchorder </Link>
-          <Link className='login-link' to={"/Login"}> Login </Link>
+          {profile ? (
+            <div>
+              {profile.name}
+              <img src={profile.picture} alt="profile picture"></img>
+              <button onClick = {()=>{  googleLogout()
+                                        setProfile(null)}}>
+                Logout
+              </button>
+            </div>
+            
+          ) : (<Link className='login-link' to={"/Login"}> Login </Link>
+          )}
+          
 
         </div>
     );
